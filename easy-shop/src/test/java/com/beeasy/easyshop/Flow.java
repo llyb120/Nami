64d02bfde39113d23549beaf8992a3c53381a9df
llyb120 <llyb120@gmail.com>
2019-05-16 10:17:19 +0800

    增加chakracore引擎

--- easy-shop/src/test/java/com/beeasy/easyshop/Flow.java
+++ easy-shop/src/test/java/com/beeasy/easyshop/Flow.java
@@ -1,8 +1,12 @@
 package com.beeasy.easyshop;
 
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.beeasy.easyshop.ctrl.user;
 import com.beeasy.web.core.Config;
 import com.beeasy.web.core.DBService;
+import com.beeasy.web.core.R;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,13 +22,22 @@ public class Flow {
 
     @Test
     public void testlogin(){
-        var query = obj(
+
+        var ret = get("/user/login",obj(
             "memberName", "myzy",
             "memberPasswd", "123456"
-        );
-        var ret = new user().login(query);
+        ));
         Assert.assertEquals(200, ret.code);
         Assert.assertNotNull(ret.data);
+    }
+
+    public String getUrl(String path){
+        return String.format("http://127.0.0.1:8082%s", path);
+    }
+
+    public R get(String url, JSONObject params){
+        var res = HttpUtil.get(getUrl(url), params);
+        return JSON.parseObject(res, R.class);
     }
 
 
