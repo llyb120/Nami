1bb3e7796624f48bd6bfdc50e5689126a3882868
llyb120 <llyb120@163.com>
2019-09-15 23:47:12 +0800

    增加js引擎

--- src/test/java/com/github/llyb120/nami/TestConf.java
+++ src/test/java/com/github/llyb120/nami/TestConf.java
@@ -1,9 +1,20 @@
 package com.github.llyb120.nami;
 
+import cn.hutool.script.ScriptUtil;
 import com.github.llyb120.nami.core.Config;
+import com.github.llyb120.nami.core.Nami;
+import com.github.llyb120.nami.json.Obj;
+import org.beetl.sql.core.SQLReady;
 import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.NativeJavaObject;
+import org.mozilla.javascript.NativeObject;
+import org.mozilla.javascript.Scriptable;
 
+import java.io.File;
 import java.io.IOException;
+
+import static com.github.llyb120.nami.ext.beetlsql.BeetlSql.sqlManager;
 
 public class TestConf {
 
@@ -18,6 +29,19 @@ public class TestConf {
             server.root.match("/test/a/b".split("/"));
         }
         long e = 1;
+    }
+
+    @Test
+    public void t2(){
+        Nami.dev();
+        sqlManager.setDefaultBeanProcessors(sqlManager.getDefaultBeanProcessors());
+        Context cx = Context.enter();
+        Scriptable scope = cx.initStandardObjects();
+
+        NativeObject ddd = (NativeObject) cx.evaluateString(scope, "(()=>{ var a = com.github.llyb120.nami.ext.beetlsql.BeetlSql.sqlManager.execute(new org.beetl.sql.core.SQLReady(\"select count(1) from t_user\"), com.github.llyb120.nami.json.Obj); return {f:a }})()", "<cmd>", 1, null);
+//        System.out.println("f");
+//        String str = (String) ddd.unwrap();
+        int a = 2;
     }
 
 
