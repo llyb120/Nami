8b082c0ed7ddd3746aa517e80b4199e1c4581d40
llyb120 <llyb120@gmail.com>
2019-10-13 12:06:38 +0800

    增加server的关闭

--- src/main/java/com/github/llyb120/nami/bean/OnVersion.java
+++ src/main/java/com/github/llyb120/nami/bean/OnVersion.java
@@ -1,4 +1,9 @@
 package com.github.llyb120.nami.bean;
 
+import java.lang.annotation.Inherited;
+
+@Inherited
 public @interface OnVersion {
+    String name() default "default";
+    String version() default "1.0";
 }
--- src/main/java/com/github/llyb120/test/Server.java
+++ src/main/java/com/github/llyb120/test/Server.java
@@ -1,5 +1,29 @@
 package com.github.llyb120.test;
 
-@com.github.llyb120.nami.bean.Server(packages = "com.github.llyb120")
+import com.github.llyb120.nami.bean.OnCreate;
+import com.github.llyb120.nami.bean.OnDestroy;
+import com.github.llyb120.nami.bean.OnVersion;
+
+import static com.github.llyb120.nami.log.Log.info;
+
+@OnVersion(name = "dev", version = "1.0")
+@com.github.llyb120.nami.bean.Server(packages = "com.github.llyb120", port = 8084)
 public class Server {
+
+    @OnVersion(name = "dev", version = "1.0")
+    @OnCreate
+    public void start(){
+        info("我是调试环境");
+    }
+
+    @OnVersion(name = "prod", version = "2.0")
+    @OnCreate
+    public void start2(){
+        info("我是生产环境");
+    }
+
+    @OnDestroy
+    public void end(){
+
+    }
 }
